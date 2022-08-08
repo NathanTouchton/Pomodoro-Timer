bf741d706b0103043525aba8ccdd129aa0d4c6f5
Nathan Touchton <99927853+NathanTouchton@users.noreply.github.com>
2022-08-07 21:06:17 -0500

    Add files via upload

--- Pomodoro/main.py
+++ Pomodoro/main.py
@@ -1,10 +1,9 @@
-
+from tkinter import Tk, Label, Button, Canvas, PhotoImage
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
-FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
@@ -16,3 +15,20 @@ LONG_BREAK_MIN = 20
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
 # ---------------------------- UI SETUP ------------------------------- #
+
+window = Tk()
+window.title("Pomodoro Timer")
+window.config(padx=100, pady=50, bg=YELLOW)
+
+image = PhotoImage(file="tomato.png")
+
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+canvas.create_image(100, 112, image=image)
+canvas.create_text(100, 130, text="00:00", fill="white", font=("Arial", 30, "bold"))
+canvas.pack()
+
+# use fg
+# copy checkmark
+# use grid instead of pack
+
+window.mainloop()
