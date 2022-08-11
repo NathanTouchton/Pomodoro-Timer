000b6503d9302c517e08bff0d3b5c3c933d25c85
Nathan Touchton <99927853+NathanTouchton@users.noreply.github.com>
2022-08-10 22:31:34 -0500

    Add files via upload

--- Pomodoro/main.py
+++ Pomodoro/main.py
@@ -1,4 +1,5 @@
 from tkinter import Tk, Label, Button, Canvas, PhotoImage
+from math import floor
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -8,20 +9,25 @@ WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 
-# ---------------------------- TIMER RESET ------------------------------- # 
+# ---------------------------- TIMER RESET ------------------------------- #
 
-# ---------------------------- TIMER MECHANISM ------------------------------- # 
+# ---------------------------- TIMER MECHANISM ------------------------------- #
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+def start_timer():
+    countdown(5 * 60)
+
+def countdown(count):
+    count_min = floor(count / 60)
+    count_sec = count % 60
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
+    if count > 0:
+        window.after(1000, countdown, count - 1)
 
 # ---------------------------- UI SETUP ------------------------------- #
 
 window = Tk()
 window.title("Pomodoro Timer")
 window.config(padx=100, pady=50, bg=YELLOW)
-
-start_button = Button(text="Start")
-start_button.grid(column=0, row=2)
 
 timer_label = Label(text="Timer", font=("Arial", 35), fg=GREEN, bg=YELLOW)
 timer_label.grid(column=1, row=0)
@@ -36,9 +42,10 @@ image = PhotoImage(file="tomato.png")
 
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 canvas.create_image(100, 112, image=image)
-canvas.create_text(100, 130, text="00:00", fill="white", font=("Arial", 30, "bold"))
+timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=("Arial", 30, "bold"))
 canvas.grid(column=1, row=1)
 
-
+start_button = Button(text="Start", command=start_timer)
+start_button.grid(column=0, row=2)
 
 window.mainloop()
