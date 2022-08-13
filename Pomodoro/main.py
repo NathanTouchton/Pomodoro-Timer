f106140be753cceabfbe981ca46085ceb53451b2
Nathan Touchton <99927853+NathanTouchton@users.noreply.github.com>
2022-08-12 22:36:00 -0500

    Add files via upload

--- Pomodoro/main.py
+++ Pomodoro/main.py
@@ -9,10 +9,17 @@ WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 REPS = 0
+CHECKMARK = "✓"
 
 # ---------------------------- TIMER RESET ------------------------------- #
 
-# ---------------------------- TIMER MECHANISM ------------------------------- #
+def reset_timer():
+    global CHECKMARK
+    CHECKMARK = "✓"
+    timer_label.config(text="Timer", fg=GREEN)
+    canvas.itemconfig(timer_text, text="00:00")
+
+# --------------------------- TIMER MECHANISM ------------------------------ #
 
 def countdown(count):
     count_min = floor(count / 60)
@@ -26,6 +33,7 @@ def countdown(count):
     if count > 0:
         window.after(1000, countdown, count - 1)
     elif count == 0:
+        add_checkmark()
         start_timer()
 
 # def start_timer():
@@ -57,6 +65,10 @@ def timer_label_func(reps_count):
     elif reps_count % 2 != 0:
         timer_label.config(text="Work.", fg=GREEN)
 
+def add_checkmark():
+    global CHECKMARK
+    CHECKMARK += "✓"
+
 # ---------------------------- UI SETUP ------------------------------- #
 
 window = Tk()
@@ -66,16 +78,17 @@ window.config(padx=100, pady=50, bg=YELLOW)
 timer_label = Label(text="Timer", font=("Arial", 35), fg=GREEN, bg=YELLOW)
 timer_label.grid(column=1, row=0)
 
-checkmark = Label(text="✓", font=("Arial", 15, "bold"), fg=GREEN, bg=YELLOW)
-checkmark.grid(column=1, row=3)
+checkmark_label = Label(text=CHECKMARK, font=("Arial", 15, "bold"), fg=GREEN, bg=YELLOW)
+checkmark_label.grid(column=1, row=3)
 
-reset_button = Button(text="Reset")
+reset_button = Button(text="Reset", command=reset_timer)
 reset_button.grid(column=2, row=2)
 
 image = PhotoImage(file="tomato.png")
 
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 canvas.create_image(100, 112, image=image)
+
 timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=("Arial", 30, "bold"))
 canvas.grid(column=1, row=1)
 
